11cfa2f19fa2994e0e7af167442779041aaf53ff
leo <qduwml@163.com>
2026-04-14 21:05:55 +0800

    feat: 添加saveTicket接口，实现ticket保存功能

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   // 例如 https://www.tms.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.tms.vip/admin/，则设置 baseUrl 为 /admin/。
-  publicPath: "/gkj/",
+  publicPath: "/plugin/",
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
   outputDir: "dist",
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
